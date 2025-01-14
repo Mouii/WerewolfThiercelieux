2de22a8b08a4bfa43005e386675456cb426fd75e
spr2-dev <spr2-dev@gmail.com>
2025-01-14 21:41:05 +0100

    Remove default code, add image load example
    Removed the default code in the Main class generated by Intellij.
    Added an example method that loads a file from the resources directory
    and prints its length.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.MissingResourceException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello, World!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            loadImageResourceExample("images/characters/Werewolf.jpg");
+        } catch (IOException e) {
+            System.err.println("Failed to read resource file!");
+        }
+    }
+
+    // TODO: Offload this to a class and make it return the actual image, somehow
+    /**
+     * Example method to load an image from the Resources directory.
+     * @throws IOException If the file could not be read.
+     */
+    private static void loadImageResourceExample(String path) throws IOException {
+        Class<Main> mainClass = Main.class;
+
+        try (InputStream imageStream = mainClass.getResourceAsStream(path)) {
+            if (imageStream == null) {
+                throw new MissingResourceException("Image not found", mainClass.getName(), path);
+            }
+
+            // Print the stream (= file) size to show the load was successful
+            System.out.printf("File size: %d bytes%n", imageStream.available());
         }
     }
 }
