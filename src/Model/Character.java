7234c877c252a9da508b58ee19a099776ff06d91
Kévin Prud'homme <kevin.prudhomme555@gmail.com>
2025-01-17 23:33:44 +0100

    Adding controller and improve character

--- src/Model/Character.java
+++ src/Model/Character.java
@@ -4,60 +4,78 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public abstract class Character {
 
-    protected final String _name;
+    protected final String name;
 
-    protected Image _image;
+    protected Image image;
 
-    protected String _description;
+    protected String description;
 
-    protected boolean _nocturnal = false;
+    protected boolean nocturnal = false;
 
-    protected PowerEnum _power = PowerEnum.Constant;
+    protected PowerEnum power = PowerEnum.Constant;
 
     public Character(String name, String description, boolean isNocturnal, PowerEnum powerType, String imagePath) {
-        _name = name;
-        _description = description;
-        _nocturnal = isNocturnal;
-        _power = powerType;
+        this.name = name;
+        this.description = description;
+        nocturnal = isNocturnal;
+        power = powerType;
 
         //Handling image
         File file = new File(imagePath);
 
         try {
-            _image = ImageIO.read(file);
+            image = ImageIO.read(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public String getName() {
-        return _name;
+        return name;
     }
 
     public Image getImage() {
-        return _image;
+        return image;
     }
 
     public String getDescription() {
-        return _description;
+        return description;
     }
 
     public boolean isNocturnal() {
-        return _nocturnal;
+        return nocturnal;
     }
 
     public void changeNocturnal() {
-        _nocturnal = !_nocturnal;
+        nocturnal = !nocturnal;
     }
 
     public PowerEnum getPower() {
-        return _power;
+        return power;
     }
 
     public void setPower(PowerEnum power) {
-        _power = power;
+        power = power;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Character character = (Character) o;
+        return nocturnal == character.nocturnal &&
+                name.equals(character.name) &&
+                image.equals(character.image) &&
+                description.equals(character.description) &&
+                power == character.power;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, image, description, nocturnal, power);
     }
 }
